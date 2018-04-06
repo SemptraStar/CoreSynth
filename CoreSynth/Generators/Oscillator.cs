842d9f52d8cc3b0cff844d6f23f7b665b0a9a8b9
oleksii.sachek <alexeycacheknew@gmail.com>
2018-04-06 15:09:29 +0300

    Base commit

---
+++ CoreSynth/Generators/Oscillator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreSynth.Generators
+{
+    public class Oscillator
+    {
+        public float Amplitude { get; set; } = 50;
+
+        public float SampleRate { get; set; } = 44100;
+
+        private float _frequency;
+        public float Frequency
+        {
+            get
+            {
+                return _frequency;
+            }
+            set
+            {
+                _frequency = value;
+
+                AngularFrequency = 2 * (float)Math.PI * _frequency;
+            }
+        }
+
+        public float AngularFrequency { get; private set; }
+
+        public float Phase { get; set; } = 0;
+
+        public sbyte[] Buffer { get; set; }
+
+        public Oscillator(int size)
+        {
+            Buffer = new sbyte[size];
+        }
+
+        public sbyte[] GenerateSine()
+        {
+            for (int i = 0; i < Buffer.Length; i++)
+            {
+                Buffer[i] = Convert.ToSByte(Amplitude * Math.Sin(AngularFrequency * i / SampleRate + Phase));
+            }
+
+            return Buffer;
+        }
+
+        public Oscillator AddWave(Oscillator other)
+        {
+            for (int i = 0; i < Buffer.Length; i++)
+            {
+                sbyte b1 = Buffer[i];
+                sbyte b2 = other.Buffer[i];
+                Buffer[i] = Convert.ToSByte((Buffer[i] / 2) + (other.Buffer[i] / 2));
+            }
+
+            return this;
+        }
+
+        public byte[] GetByteWave()
+        {
+            return Buffer.Select(x => Convert.ToByte(Math.Abs(x))).ToArray();
+        }
+    }
+}
---
+++ CoreSynth/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using CoreSynth.Generators;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoreSynth
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+
+        public Oscillator Osc1 = new Oscillator(100000);
+
+        public Oscillator Osc2 = new Oscillator(100000);
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Osc1.Frequency = Convert.ToSingle(FrequencyInputTextBox1.Text);
+            Osc2.Frequency = Convert.ToSingle(FrequencyInputTextBox2.Text);
+
+            Osc2.Phase = (float)Math.PI / 3;
+
+            Osc1.GenerateSine();
+            Osc2.GenerateSine();
+
+            Osc1.AddWave(Osc2);
+
+            Play(Osc1.GetByteWave());
+        }
+
+        private void Play(byte[] buffer)
+        {
+            var source = new RawSourceWaveStream(
+                new MemoryStream(buffer),
+                new WaveFormat()
+                );
+
+            var wv = new WaveOutEvent();
+            wv.Init(source);
+            wv.Play();
+        }
+    }
+}
